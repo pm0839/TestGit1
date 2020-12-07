e2b698f8496aed10aeb588440306f0f08b987acd
pm0839 <75485310+pm0839@users.noreply.github.com>
2020-12-07 16:45:09 +0400

    доб 23

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -7,7 +7,7 @@ namespace TestGit
         static void Main(string[] args)
         {
             //            Console.WriteLine("Hello World 111!");
-            Console.WriteLine("Hello!");
+            Console.WriteLine("Hello 23!");
             Console.WriteLine("World 111!");
         }
     }
